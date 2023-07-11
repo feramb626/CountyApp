bf0a3eb0fa4de74c10ddc3f2d6bd62cd6747e8a1
feramb626 <nando626@gmail.com>
2023-07-11 10:37:22 -0300

    hasta pais hecho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharModule } from './shared/shar.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharModule
   ],
   providers: [],
   bootstrap: [AppComponent]
